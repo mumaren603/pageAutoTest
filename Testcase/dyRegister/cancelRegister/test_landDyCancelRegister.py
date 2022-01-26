9be6f71816782ff867ac426e18c7272bd69d60b2
liulinshan <liulinshan@ztgeo.com.cn>
2022-01-26 14:20:51 +0800

    新增抵押注销房地流程和净地流程

---
+++ Testcase/dyRegister/cancelRegister/test_landDyCancelRegister.py
@@ -0,0 +1,80 @@
+import pytest,os
+from init.dataInit import dataInit
+from pageObject.taskCenter import taskCenter
+from pageObject.queryFunc import queryFunc
+from pageObject.sjdPage import sjdPage
+from pageObject.sqrqkPage import sqrqkPage
+from pageObject.sqbPage import sqbPage
+from pageObject.bdcjbxxPage import bdcjbxxPage
+from pageObject.sflzbPage import sflzbPage
+from pageObject.blyjPage import blyjPage
+from pageObject.htxxPage import htxxPage
+from utils.getTestdata import getTestcaseData,getTestdataPath
+from pageObject.submitPage import submitPage
+from pageObject.logout import logout
+from dataCheck.dataResCheck import dataResCheck
+from Common.LogFunc import loggerConf
+
+logger = loggerConf().getLogger()
+
+@pytest.mark.test
+class Test_landDyCancelRegister():
+    def setup(self):
+        '''初始化用户数据获取'''
+        current_file_path = os.path.abspath(__file__).replace('\\','/')
+        self.data = getTestcaseData(getTestdataPath(current_file_path))
+
+    def test_landDyCancelRegister(self,login,cmdopt):
+        '''
+        :流程 抵押权--注销登记--净地抵押注销
+        '''
+        self.driver = login[0]
+        dbInfo = login[1]
+        # 获取办件数据
+        bdcdyh = dataInit().getLandDyRegisterData()
+        logger.debug("<--------抵押权--注销登记--净地抵押注销start-------->")
+        logger.debug("<--------界面操作start-------->")
+
+        # 办件中心
+        taskCenter(self.driver).common()
+        # 选择流程
+        taskCenter(self.driver).chooseNode(self.data)
+        # 发起查询
+        queryFunc(self.driver).query(bdcdyh, self.data)
+        # 收件单
+        sjdPage(self.driver).sjdHandle(self.data)
+        # 申请人情况
+        sqrqkPage(self.driver).sqrqkHandle(self.data)
+        # 申请表
+        sqbPage(self.driver).sqbHandle(self.data)
+        # 不动产基本信息
+        bdcjbxxPage(self.driver).bdcjbxxHandle(self.data)
+        # 收费领证表
+        # sflzbPage(self.driver).sflzbHandle()
+        # 办理意见表
+        blyjPage(self.driver).blyjHandle()
+        # 受理
+        submitPage(self.driver).slHandle()
+        # 登簿
+        submitPage(self.driver).dbHandle(bdcdyh, self.data)
+        logger.debug("<--------界面操作end------->")
+
+        # 数据库校验
+        try:
+            logger.debug("<--------归档数据检查start-------->")
+            resDataCheck = dataResCheck().dyCancelRegisterDataCheck(bdcdyh, self.data)
+            assert resDataCheck
+            logger.debug("<--------归档数据检查end-------->")
+        except AssertionError:
+            raise
+        logger.debug("<--------抵押权--注销登记--净地抵押注销end-------->")
+
+    def teardown(self):
+        logger.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>测试用例执行end<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
+        # 退出系统
+        logout(self.driver).logout()
+        # 退出浏览器
+        self.driver.quit()
+
+if __name__ == '__main__':
+    pytest.main(['-v', 'test_landDyCancelRegister'])
