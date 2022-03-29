744b7f8515eb2a022e5dc3e9c594a59c7898703c
liulinshan <liulinshan@ztgeo.com.cn>
2022-03-29 10:52:55 +0800

    引入日志模块，增加日志说明

--- Testcase/processCenter/myTask/newTask/conftest.py
+++ Testcase/processCenter/myTask/newTask/conftest.py
@@ -1,19 +1,22 @@
 import pytest
 from Common.ToolsForOpertion import WebTools
 from pageObject.taskCenter import taskCenter
+from Common.LogFunc import loggerConf
+
+logger = loggerConf().getLogger()
 
 @pytest.fixture(scope="function")
 def openProcessCenter(login):
     '''
     :param login: 装饰器 调取登录方法（login(fixture)-->conftest）
-    :return: driver对象和数据库配置信息
+    :return: driver对象
     :action: 模块下每个用例执行前需打开办件中心模块，执行后关闭办件中心
     '''
-    print("打开办件中心模块")
     driver = login
-    # 打开查询中心
+
+    logger.debug("打开办件中心模块")
     taskCenter(driver).workCenter()
-    yield login
-    # 关闭办件中心
+    yield driver
     WebTools(driver).is_element_exist("//i[@class='icon-close-round cFNvY7v']")
     WebTools(driver).mouse_click('xpath',"//i[@class='icon-close-round cFNvY7v']")
+    logger.debug("关闭办件中心模块")
